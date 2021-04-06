9cc973b7592e537a2211e2d2b749acfbdcd3af3b
Matias <lxpirez133@gmail.com>
2021-04-06 01:22:12 +0000

    exercise

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,9 @@
 # puppet
+file_line { 'identity':
+  ensure => present,
+  line   => 'IdentityFile ~/.ssh/holberton',
+  path   => '/etc/ssh/ssh_config',
+}
 file_line {'password auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
